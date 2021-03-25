f44092357b49559df439240df88abc6e3b12d8a4
Muni10 <sten.ivar.rokke@gmail.com>
2021-03-25 13:52:12 +0100

    Added prometheus pushgateway

--- check_clusters.sh
+++ check_clusters.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
+# Script for checking external communication from gcp clusters
 
-clusters=("ci-gcp" "dev-gcp")
-for cluster in $clusters; do
-  #result=$(curl -s https://testrig.${cluster}.nais.io/adhoc > ${cluster}.json)
-  success=$(cat ${cluster}.json |jq -r .success)
+export no_proxy=".nais.io,prometheus-pushgateway.nais.adeo.no"
+declare -a clusters=("dev-gcp" "prod-gcp")
 
-  if [[ "${success}" == "false" ]]; then
-    cat ${cluster}.json | jq -r .markdown | grep ":red_circle:"
-  fi
+for cluster in "${clusters[@]}"; do
+  result=$(curl -s https://testrig.${cluster}.nais.io/adhoc > ${cluster}.json)
 
+  while read -r httpstest; do
+    testname=$(echo ${httpstest} | cut -d"(" -f1 |sed 's/ -> https:\/\//_/g' | tr '.' '_'| tr '-' '_')
+    prefix=$(echo ${cluster}|cut -d- -f1)
 
+    if [ "$(echo ${httpstest} |grep ':red_circle:')" != "" ]; then
+      echo "${prefix}_${testname} 1" | curl -k --data-binary @- https://prometheus-pushgateway.nais.adeo.no/metrics/job/testrig/cluster/prod-fss
+    else
+      echo "${prefix}_${testname} 0" | curl -k --data-binary @- https://prometheus-pushgateway.nais.adeo.no/metrics/job/testrig/cluster/prod-fss
+    fi
+
+  done < <(cat ${cluster}.json | jq -r .markdown | grep https)
 done
