437bd485134584d32f5885a37584eaf0a21b1bc7
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-10-18 15:42:27 +0200

    Use new synchronous storage tests

--- src/storage.js
+++ src/storage.js
@@ -4,12 +4,10 @@ const {AbortController} = require("abort-controller")
 const requestTimeout = 10 * 1000
 
 exports.runStorageTests = (tests, storageGauge) => {
-    const testString = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 4)
     tests.forEach(async (test) => {
         let read
         try {
-            await writeStorage(test, testString)
-            read = await readStorage(test, testString)
+            read = await testStorage(test)
             storageGauge
                 .labels(test.name)
                 .set(read ? 1 : 0)
@@ -21,34 +19,7 @@ exports.runStorageTests = (tests, storageGauge) => {
     })
 }
 
-const writeStorage = async (test, testString) => {
-    const controller = new AbortController()
-    setTimeout(() => {
-            controller.abort()
-        }, requestTimeout,
-    )
-
-    let opts = {
-        method: "post",
-        signal: controller.signal,
-        body: testString
-    }
-
-    try {
-        const response = await fetch(test.writeurl, opts)
-        if (response.status === 201) {
-            return true
-        } else {
-            const text = await response.text()
-            console.error(`${new Date().toUTCString()} - ${test.name}, writing: expected 201 but got ${response.status}: ${text}`)
-            return false
-        }
-    } catch (err) {
-        console.error(`${new Date().toUTCString()} - ${test.name}: writing value to ${test.writeurl}: ${err}`)
-        return false
-    }
-}
-const readStorage = async (test, testString) => {
+const testStorage = async (test) => {
     const controller = new AbortController()
     setTimeout(() => {
             controller.abort()
@@ -56,15 +27,15 @@ const readStorage = async (test, testString) => {
     )
 
     try {
-        const response = await fetch(test.readurl, controller.signal)
+        const response = await fetch(test.testUrl, controller.signal)
         const text = await response.text()
-        if (text === testString) return true
+        if (response.status === 200) return true
         else {
-            console.error(`${new Date().toUTCString()} - ${test.name}, reading: expected ${testString} but got ${response.status}: ${text}`)
+            console.error(`${new Date().toUTCString()} - ${test.name}: code(${response.status}): err ${text}`)
             return false
         }
     } catch (err) {
-        console.error(`${new Date().toUTCString()} - ${test.name}: reading value from ${test.url}: ${err}`)
+        console.error(`${new Date().toUTCString()} - ${test.name}: url(${test.testUrl}): ${err}`)
         return false
     }
 }
